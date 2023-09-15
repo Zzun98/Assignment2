8fa473bc31fad23aa810c409c4a4b76266e9cf95
Zzun98 <sunnylucie98@gmail.com>
2023-09-15 10:50:55 +1000

    Initial Commit

---
+++ Assignment2/Assignment2App.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment2App.swift
+//  Assignment2
+//
+//  Created by Sunjun Kwak on 15/9/2023.
+//
+
+import SwiftUI
+
+@main
+struct Assignment2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Assignment2/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Assignment2
+//
+//  Created by Sunjun Kwak on 15/9/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
